108fc52f9591175392533b897ebc7392b949bf68
pranulkbv28 <28bvpranaav@gmail.com>
2025-03-02 00:42:34 +0530

    set up backend server with Express, add dotenv and CORS support, and create initial app structure

---
+++ backend/app.js
@@ -0,0 +1,16 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+export default app;
